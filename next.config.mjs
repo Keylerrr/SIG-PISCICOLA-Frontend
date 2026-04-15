c3abccbfea7baf5829394b00f3e08d3cb07f7fe6
Andres Monsalve <andresfelipemope@ufps.edu.co>
2026-04-14 19:03:27 -0500

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   /* config options here */
   //reactCompiler: true,
   output: 'export',
+  basePath: '/SIG-Piscicola',
+  assetPrefix: '/SIG-Piscicola/',
 };
 
 export default nextConfig;
